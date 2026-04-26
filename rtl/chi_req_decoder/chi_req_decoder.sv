91ba78d9b734ff95d50eb0dc2d8fe73a186f2e0b
Venkata Karthik Dodda <vdodda@asu.edu>
2026-04-25 20:42:21 -0700

    Added FSM logic for credit count

--- rtl/chi_req_decoder/chi_req_decoder.sv
+++ rtl/chi_req_decoder/chi_req_decoder.sv
@@ -41,17 +41,108 @@ localparam WRITENOSNPPTL  = 7'b0011100;
 localparam WRITENOSNPFULL = 7'b0011101;
 
 // Internal signals
-logic[2:0] credit_counter;
-logic credit_over_flag;
+logic[2:0] credit_count;
+logic init_credit_over;
 
 // Field extraction - only when flit is valid
-assign req_is_read = (req_opcode == READNOSNP);
-assign req_is_write = (req_opcode == WRITENOSNPFULL) | (req_opcode == WRITENOSNPPTL);
+always_comb begin
+    if(rxreqflitv)begin //HN sends this signal and once we recieve the actual flit, we assign our o/ps in SN-F
+
+    assign req_qos = rxreqflit[3:0];
+    assign req_tgt_id = rxreqflit[10:4];
+    assign req_src_id = rxreqflit[17:11];
+    assign req_txn_id = rxreqflit[29:18];
+    assign req_opcode = rxreqflit[56:50];
+    assign req_size = rxreqflit[60:58];
+    assign req_addr = rxreqflit[115:64];
+    assign req_allow_retry = rxreqflit[120];
+    assign req_order = rxreqflit[122:121];
+    assign req_mem_attr = rxreqflit[130:127];
+    assign req_exp_comp_ack = rxreqflit[141];
+        
+    end
+
+    else begin
+
+    assign req_qos = 0;
+    assign req_tgt_id = 0;
+    assign req_src_id = 0;
+    assign req_txn_id = 0;
+    assign req_opcode = 0;
+    assign req_size = 0;
+    assign req_addr = 0;
+    assign req_allow_retry = 0;
+    assign req_order = 0;
+    assign req_mem_attr = 0;
+    assign req_exp_comp_ack = 0;
+        
+    end
+end
 
 // Opcode decoding
-// TODO: add opcode decode
+assign req_is_read =  rxreqflitv && (req_opcode == READNOSNP);
+assign req_is_write = rxreqflitv && ((req_opcode == WRITENOSNPFULL) || (req_opcode == WRITENOSNPPTL));
+
+assign req_valid = req_is_read || req_is_write:
+
 
 // Credit return logic
-// TODO: add credit logic
+
+typedef enum logic[1:0] {INIT = 2'b00, NORMAL = 2'b01} state_t;
+
+state_t credit_state;
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if(!rst_n)begin
+
+        credit_state<=INIT;
+        credit_count<=INIT_CREDIT;
+        rxreqlcrdv<=0;
+        init_credit_over<=0;
+
+    end
+
+    else begin
+        case (credit_state)
+        
+            INIT:begin
+                if(credit_count>0)begin
+
+                    rxreqlcrdv<=1; //if this is high, it means SN-F is sending credits to HN and using that, HN will send a Flit
+                    credit_count<=credit_count-1;
+
+                end
+
+                else begin
+                    
+                    rxreqlcrdv<=0;
+                    init_credit_over<=1;
+                    credit_state<=NORMAL;
+
+                end
+            end
+
+            NORMAL:begin
+                // Return one credit to HN the cycle after receiving a flit
+                if(rxreqflitv)begin
+                    rxreqlcrdv<=1;
+                end
+
+                else begin
+                    rxreqlcrdv<=0;
+                end
+            end
+
+            default:begin
+
+                credit_state<=INIT;
+                rxreqlcrdv<=0;
+
+            end
+
+        endcase
+    end
+    
+end
 
 endmodule
